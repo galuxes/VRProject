4991f7304710d6c3d591a4f5274199d2b1a26645
Austin J. Baird <90073330+galuxes@users.noreply.github.com>
2023-10-11 17:24:01 -0400

    spear?

---
+++ Tk VR/Assets/Spear.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.XR.Interaction.Toolkit;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Spear : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    public bool isFlying = false;
+    
+    private void Update()
+    {
+        if (isFlying)
+        {
+            transform.up = _rb.velocity.normalized;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            isFlying = false;
+            _rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFlying = true;
+        }
+    }
+}
